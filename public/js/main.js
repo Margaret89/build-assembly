d98b3cb842b0e3d34c5b01a5984cab32f6d7bcaa
margarita <match14@yandex.ru>
2018-05-17 18:03:45 +0300

    Анимация слайдера

--- public/js/main.js
+++ public/js/main.js
@@ -10,13 +10,24 @@ $(document).ready(function () {
 			dots: false,
 			navText : ["<i></i><i></i>","<i></i><i></i>"],
 			onInitialized: function(e) {
-				$('.js-t-counter').html('1&nbsp;&nbsp;/&nbsp;&nbsp;' + this.items().length)
+				$('.js-t-counter').html('1&nbsp;&nbsp;/&nbsp;&nbsp;' + this.items().length);
+				$('.owl-item.active').addClass('anim-text');
 			}
 		});
 
 		// Подсчет слайдов при перелистывании
 		$topSlider.on('changed.owl.carousel', function(e) {
-			$('.js-t-counter').html(++e.item.index  + '&nbsp;&nbsp;/&nbsp;&nbsp;' + e.item.count)
+			$('.js-t-counter').html(++e.item.index  + '&nbsp;&nbsp;/&nbsp;&nbsp;' + e.item.count);
+			var current = e.item.index - 1;
+			var $elCurrent = $(e.target).find(".owl-item").eq(current);
+			if ($elCurrent.hasClass("anim-text")) {
+				$elCurrent.removeClass('anim-text');
+				$elCurrent.addClass('no-anim-text');
+			}else if($elCurrent.hasClass("no-anim-text")){
+			}else{
+				$elCurrent.addClass('anim-text');
+			}
 		});
+
 	}
 });
