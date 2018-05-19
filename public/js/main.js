0c2dff42c897994ef208d5e8a3c3dcb4629eae52
Margarita <match14@yandex.ru>
2018-05-19 20:41:19 +0300

    Доверстана главная страница, сверстана страницы продукция и контакты

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 			nav: true,
 			dots: false,
 			thumbs: false,
-			navText : ["<i></i><i></i>","<i></i><i></i>"],
+			navText : ['<i></i><i></i>','<i></i><i></i>'],
 			onInitialized: function(e) {
 				$('.js-t-counter').html('1&nbsp;&nbsp;/&nbsp;&nbsp;' + this.items().length);
 				$('.owl-item.active').addClass('anim-text');
@@ -20,11 +20,11 @@ $(document).ready(function () {
 		$topSlider.on('changed.owl.carousel', function(e) {
 			$('.js-t-counter').html(++e.item.index  + '&nbsp;&nbsp;/&nbsp;&nbsp;' + e.item.count);
 			var current = e.item.index - 1;
-			var $elCurrent = $(e.target).find(".owl-item").eq(current);
-			if ($elCurrent.hasClass("anim-text")) {
+			var $elCurrent = $(e.target).find('.owl-item').eq(current);
+			if ($elCurrent.hasClass('anim-text')) {
 				$elCurrent.removeClass('anim-text');
 				$elCurrent.addClass('no-anim-text');
-			}else if($elCurrent.hasClass("no-anim-text")){
+			}else if($elCurrent.hasClass('no-anim-text')){
 			}else{
 				$elCurrent.addClass('anim-text');
 			}
@@ -32,7 +32,7 @@ $(document).ready(function () {
 
 	}
 
-	// Слайдер "о компании"
+	//  --------- Слайдер 'о компании' ---------
 	var $aboutSlider = $('.js-slider-about');
 	if ($aboutSlider.length) {
 		$('.js-slider-about').owlCarousel({
@@ -45,7 +45,7 @@ $(document).ready(function () {
 		});
 	}
 
-	// Слайдер "новости"
+	// --------- Слайдер 'новости' ---------
 	var $newsSlider = $('.js-slider-news');
 	if ($newsSlider.length) {
 		$newsSlider.owlCarousel({
@@ -56,4 +56,47 @@ $(document).ready(function () {
 			margin: 30,
 		});
 	}
+
+	// --------- Плавный переход к ссылке ---------
+	if ($('.js-link-move').length) {
+		$('body').on('click','.js-link-move', function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 1000);
+
+			// Проверка наличия анимации
+			productAnim(1);
+		});
+	}
+
+	// --------- Анимация блоков продукция ---------
+	function productAnim(delay){
+		var $productBox = $('.js-product-list');
+		if ($productBox.length) {
+			if (!$productBox.hasClass('anim')) {
+				$productBox.addClass('anim');
+
+				$('.js-product-item').each(function(indx, element){
+					$(this).css({'animation-delay': delay+'s', '-webkit-animation-delay': delay+'s'})
+					delay = parseFloat(delay);
+					delay = delay + 0.2;
+					delay = delay.toFixed(1);
+				});
+			}
+		}
+	}
+
+	// Включение анимации при скролле
+	if ($('.js-product-list').length) {
+		var topProduct = $('.js-product-list').offset().top;
+		var topProductScroll = topProduct - $('.js-product-list').outerHeight()/2;
+
+		$(window).scroll(function(){
+			if($(this).scrollTop()>=topProductScroll){
+				productAnim(0);
+			}
+		});
+	}
+
 });
